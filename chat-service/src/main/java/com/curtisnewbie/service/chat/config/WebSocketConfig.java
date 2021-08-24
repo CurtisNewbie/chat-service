a987b6c6c264e914d91b8f59a138e7250c53b92f
CurtisNewbie <zhuangyongj@gmail.com>
2021-08-24 22:07:02 +0800

    Add allowed origins for dev profile

--- chat-service/src/main/java/com/curtisnewbie/service/chat/config/WebSocketConfig.java
+++ chat-service/src/main/java/com/curtisnewbie/service/chat/config/WebSocketConfig.java
@@ -1,10 +1,12 @@
 package com.curtisnewbie.service.chat.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistration;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
 import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
@@ -20,11 +22,16 @@ public class WebSocketConfig implements WebSocketConfigurer {
     @Autowired
     private RoomMessageWebSocketHandler roomMessageWebSocketHandler;
 
+    @Value("${websocket.allowed-origins:}")
+    private String[] allowedOrigins;
+
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry webSocketHandlerRegistry) {
-        webSocketHandlerRegistry.addHandler(roomMessageWebSocketHandler, "/socket/messages")
+        WebSocketHandlerRegistration reg = webSocketHandlerRegistry.addHandler(roomMessageWebSocketHandler, "/socket/messages")
                 .addInterceptors(new HttpSessionHandshakeInterceptor())
                 .setHandshakeHandler(new DefaultHandshakeHandler());
-//                .setAllowedOrigins("http://localhost:4200");
+
+        if (allowedOrigins.length > 0)
+            reg.setAllowedOrigins(allowedOrigins);
     }
 }
