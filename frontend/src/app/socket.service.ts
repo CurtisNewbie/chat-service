a24092923f8c095ab050fa6852dcc4aaed910acc
CurtisNewbie <zhuangyongj@gmail.com>
2021-08-25 23:34:33 +0800

    Update layout

--- frontend/src/app/socket.service.ts
+++ frontend/src/app/socket.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { webSocket, WebSocketSubject } from 'rxjs/webSocket';
-import { Message } from './models/Message';
+import { Message, MessageVo } from './models/Message';
 import { NotificationService } from './notification.service';
 
 @Injectable({
@@ -13,12 +13,12 @@ export class SocketService {
    * Open websocket connection for messages
    *
    */
-  openMessageWebSocket(): WebSocketSubject<Message> {
-    let wss: WebSocketSubject<Message> = webSocket({
-      url: `ws://localhost:8081/socket/messages`,
+  openMessageWebSocket(): WebSocketSubject<MessageVo> {
+    let wss: WebSocketSubject<MessageVo> = webSocket({
+      // url: `ws://127.0.0.1:8081/socket/messages`,
       // url: `ws://${location.host}/socket/messages`,
       // url: `wss://${location.host}/socket/messages`,
-      // url: `wss://${location.host}/socket/messages`,
+      url: `wss://${location.host}/socket/messages`,
       openObserver: {
         next(event) {
           console.log('Message web socket opened');
