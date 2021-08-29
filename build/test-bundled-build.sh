9d30b2f0a749f636419e630284dbfa51f53bcaa5
CurtisNewbie <zhuangyongj@gmail.com>
2021-08-29 14:58:50 +0800

    Add script & update config for test profile

--- build/test-bundled-build.sh
+++ build/test-bundled-build.sh
@@ -7,7 +7,7 @@ if [ -d $staticpath ]; then
 fi
 
 (
-  cd "../frontend/";   
+  cd ../frontend/;   
   ng build;
 )
 
@@ -20,9 +20,13 @@ cp -r ../frontend/dist/chatappfront/* ../chat-service/src/main/resources/static/
 
 mvn clean package -f ../chat-service
 
+jarpath="../chat-service/target/chat-service-1.0.0.jar";
+
 if [ $? -eq 0 ]
 then
-  cp ../chat-service/target/chat-service-1.0.0.jar .
+    cp $jarpath .
+    ssh -l zhuangyongj 192.168.10.128 "/home/zhuangyongj/exec/chatserver/kill.sh"
+    scp $jarpath "zhuangyongj@192.168.10.128:~/exec/chatserver/chatserver.jar"
 fi
 
 if [ -d $staticpath ]; then
